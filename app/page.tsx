5688f7a1c4aa5d979902e06913077e3ed8951dbf
Sunwoo Jung <49130227+InHeritas@users.noreply.github.com>
2025-06-21 16:27:43 +0900

    update demo

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100">
       <div className="w-full mx-auto bg-white shadow-sm border-neutral-200 p-4">
-        <div className="space-y-6 text-gray-600">
+        <div className="space-y-8 text-gray-600 text-center">
           <div className="space-y-2">
             <p className="font-bold break-keep">
               콩고물과 우유가 들어간 빙수는 차게 먹어야 특별한 맛이 잘 표현된다.
@@ -26,6 +26,54 @@ export default function Home() {
           <p className="break-words">
             ❤ ♫ ☎ • ° ♨ ✈ ✣ ☏ ■ ☀ ➑ ✂ ☑ ✉ ☼ ☆ ✄ ✔ ✆ — ☁ ★ ♕ ✘ № ‰ ♠ ✪ ✝ ╳ © … ♥ ✰ † ✎ ® ¶ ♦ ✧ ‡ ✍ ™ ❆ ♣ ✦ ◑ ♀ ℮ ❅ ♤ ♡ ♪ ♂
           </p>
+
+          <hr />
+
+          <div className="space-y-4">
+            <div className="space-y-3 px-4 md:px-8">
+              <p>
+                죽는 날까지 하늘을 우러러
+                <br />
+                한 점 부끄럼이 없기를,
+                <br />
+                잎새에 이는 바람에도
+                <br />
+                나는 괴로워했다.
+                <br />
+                <br />
+                별을 노래하는 마음으로
+                <br />
+                모든 죽어가는 것을 사랑해야지
+                <br />
+                그리고 나한테 주어진 길을
+                <br />
+                걸어가야겠다.
+                <br />
+                <br />
+                오늘 밤에도 별이 바람에 스치운다.
+              </p>
+            </div>
+          </div>
+
+          <div className="space-y-4">
+            <div className="space-y-3 px-4 md:px-8">
+              <p>
+                There was nothing so VERY remarkable in that;
+                <br />
+                nor did Alice think it so VERY much out of the way
+                <br />
+                to hear the Rabbit say to itself, 'Oh dear! Oh dear! I shall be
+                late!'
+              </p>
+              <p>
+                Burning with curiosity, she ran across the field after it,
+                <br />
+                and fortunately was just in time to see it pop
+                <br />
+                down a large rabbit-hole under the hedge.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
